ea84c6b709e786ac056b9102eba6e6d4d06c036a
Yan Rabovik <yan@rabovik.ru>
2013-08-21 20:46:33 +0400

    Logging without arguments

--- Example & Tests/ATLogExample/AppDelegate.m
+++ Example & Tests/ATLogExample/AppDelegate.m
@@ -56,6 +56,9 @@
                self.window.alignmentRectInsets,
                self.window.keyWindow);
     
+    // May be used without arguments
+    ATLog();
+    
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
